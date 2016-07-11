5c7ed1720ceaacc97b3b2d912a97d9370ec16a45
nyxcodec <nyxcodec@gmail.com>
2016-07-11 04:58:12 -0700

    wog home

---
+++ wognwn.js
@@ -0,0 +1,15 @@
+//default javascript
+//document.write("yo");
+
+//self invoking function closure (for private scope)
+//(function testcode()
+//{
+	//var data = "yoyoyowazzup?"	
+	//document.getElementById("display").innerHTML=data;
+	//data = "else";
+	//document.getElementById("display").innerHTML=data;
+//})();
+
+//private scope
+//document.getElementById("display").innerHTML=data;
+//document.write("nada");
