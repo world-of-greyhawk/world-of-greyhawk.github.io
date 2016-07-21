7d20406f02dc755d1d275c2ed46eff8c38153a61
nyxcodec <nyxcodec@gmail.com>
2016-07-20 22:45:59 -0700

    Update wognwn.js

--- wognwn.js
+++ wognwn.js
@@ -1,15 +1,1 @@
-//default javascript
-//document.write("yo");
 
-//self invoking function closure (for private scope)
-//(function testcode()
-//{
-	//var data = "yoyoyowazzup?"	
-	//document.getElementById("display").innerHTML=data;
-	//data = "else";
-	//document.getElementById("display").innerHTML=data;
-//})();
-
-//private scope
-//document.getElementById("display").innerHTML=data;
-//document.write("nada");
