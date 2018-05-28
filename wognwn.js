82125145c5c5a2719f9644e07bbc94bbb21409ad
JaMiT <jt_coding@zoho.com>
2018-05-28 17:49:43 -0500

    Remove aside & prep formatting for navigation

--- wognwn.js
+++ wognwn.js
@@ -1,1 +1,20 @@
+/** Flags the specified element as having been selected. */
+function DoSelect(elem)
+{
+	const SELECTED = 'target';
+
+	let cl_list = elem.classList;
+	// Performance quit if already selected.
+	if ( cl_list.contains(SELECTED) )
+		return false;
+
+	// Unflag the old selection.
+	let oldNodes = elem.parentNode.getElementsByClassName(SELECTED);
+	for ( let i = oldNodes.length; i-- > 0; )
+		oldNodes[i].classList.remove(SELECTED);
+
+	// Flag the new selection.
+	cl_list.add(SELECTED);
+	return true;
+}
 
